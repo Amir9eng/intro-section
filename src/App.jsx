6c118989a53ba2513aa21aa7b10ac48aad7cbd69
Amir9eng <amirmukhtarme@gmail.com>
2022-07-28 22:29:29 +0100

    mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import logo from './images/logo.svg'
 import hero from './images/image-hero-desktop.png'
+import mobile from './images/image-hero-mobile.png'
 import arrowup from './images/icon-arrow-up.svg'
 import arrowdown from './images/icon-arrow-down.svg'
 import databiz from './images/client-databiz.svg'
 import audiophile from './images/client-audiophile.svg'
 import maker from './images/client-maker.svg'
 import meet from './images/client-meet.svg'
+import todolist from './images/icon-todo.svg'
+import calendar from './images/icon-calendar.svg'
+import reminder from './images/icon-reminders.svg'
+import planning from './images/icon-planning.svg'
+
 import { useState } from 'react'
 
 function App () {
   const [div, setDiv] = useState(false)
+  const [company, setCompany] = useState(false)
+
   return (
     <div className='App'>
       <section>
@@ -19,13 +27,48 @@ function App () {
             <div>
               <img className='image' src={logo} alt='' />
             </div>
-            <div className='features'>
+            <div className='features' onClick={() => setDiv(prev => !prev)}>
               <p>Features</p>
-              <img src={arrowdown} alt='arrowdown' className='arrowdown' />
+              {div ? (
+                <img src={arrowup} alt='arrowup' className='arrowdown' />
+              ) : (
+                <img src={arrowdown} alt='arrowdown' className='arrowdown' />
+              )}
+              {div && (
+                <div className='dropdown'>
+                  <div className='option'>
+                    <img src={todolist} alt='Todolist' />
+                    <span>Todo List</span>
+                  </div>
+                  <div className='option'>
+                    <img src={calendar} alt='calendar' />
+                    <span>Calendar</span>
+                  </div>
+                  <div className='option'>
+                    <img src={reminder} alt='reminder' />
+                    <span>Reminders</span>
+                  </div>
+                  <div className='option'>
+                    <img src={planning} alt='planning' />
+                    <span>Planning</span>
+                  </div>
+                </div>
+              )}
             </div>
-            <div className='company'>
+            <div className='company' onClick={() => setCompany(prev => !prev)}>
               <p>Company</p>
-              <img src={arrowdown} alt='arrowdown' className='arrowdown' />
+              {company ? (
+                <img src={arrowup} alt='arrowup' className='arrowdown' />
+              ) : (
+                <img src={arrowdown} alt='arrowdown' className='arrowdown' />
+              )}
+              {company && (
+                <div className='dropdown history'>
+                  <span>History</span>
+                  <span>Our Team</span>
+                  <span>Blog</span>
+                </div>
+              )}
             </div>
             <p>Careers</p>
             <p>About</p>
@@ -42,20 +85,16 @@ function App () {
           {' '}
           <div className='remote'>
             <div>
+              <div className='mobile'>
+                <img src={mobile} alt='' />
+              </div>
               <h1>Make remote work</h1>
               <p>
                 Get your team in sync, no matter your location. Streamline
                 processes, create team rituals, and watch productivity soar.
               </p>
             </div>
-            <button onClick={() => setDiv(prev => !prev)} className='button'>
-              Learn more
-            </button>
-            {div && (
-              <div
-                style={{ width: '20px', height: '20px', background: 'red' }}
-              ></div>
-            )}
+            <button className='button'>Learn more</button>
             <div className='down'>
               <img src={databiz} alt='databiz' />
               <img src={audiophile} alt='audiophile' />
